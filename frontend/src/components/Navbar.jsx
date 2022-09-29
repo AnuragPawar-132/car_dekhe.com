8292866a7285310417ff41fdcefd596c1281c26a
AnuragPawar-132 <anuragpawar.me@gmail.com>
2022-09-29 08:40:50 +0530

    removing previous navbar design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,83 +1,10 @@
-import { Box, HStack, Link, Button } from '@chakra-ui/react'
 import React from 'react'
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider
-  } from '@chakra-ui/react'
-  import {ChevronDownIcon} from "@chakra-ui/icons"
 
 const Navbar = () => {
   return (
-    <HStack w="100%"  >
-        
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >Find New Car</MenuButton>
-            <MenuList>
-                <MenuItem>Cars</MenuItem>
-                <MenuItem>Compare Cars</MenuItem>
-                <MenuItem>Upcoming Cars</MenuItem>
-                <MenuItem>Car Loan Calculator</MenuItem>
-            </MenuList>
-        </Menu>
+    <div>
 
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >Find New Bike</MenuButton>
-            <MenuList>
-                <MenuItem>Bikes</MenuItem>
-                <MenuItem>Compare Bikes</MenuItem>
-                <MenuItem>Upcoming Bikes</MenuItem>
-                <MenuItem>Bikes Loan Calculator</MenuItem>
-            </MenuList>
-        </Menu>
-        
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >News</MenuButton>
-            <MenuList>
-                <MenuItem>Cars</MenuItem>
-                <MenuItem>Bikes</MenuItem>
-                <MenuItem>Industry</MenuItem>
-            </MenuList>
-        </Menu>
-
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >Reviews</MenuButton>
-            <MenuList>
-                <MenuItem>Cars</MenuItem>
-                <MenuItem>Bikes</MenuItem>
-            </MenuList>
-        </Menu>
-
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >News</MenuButton>
-            <MenuList>
-                <MenuItem>Cars</MenuItem>
-                <MenuItem>Bikes</MenuItem>
-                <MenuItem>Industry</MenuItem>
-            </MenuList>
-        </Menu>
-
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >Galleries</MenuButton>
-            <MenuList>
-                <MenuItem>Images</MenuItem>
-            </MenuList>
-        </Menu>
-
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >Readers Corner</MenuButton>
-            <MenuList>
-                <MenuItem>Travel Blogs</MenuItem>
-                <MenuItem>Blogs</MenuItem>
-            </MenuList>
-        </Menu>
-
-    </HStack>
+    </div>
   )
 }
 
